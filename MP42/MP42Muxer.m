c8ca201784236aa811f9cfd42b679026b3c140ac
Damiano Galassi <damiog@gmail.com>
2016-08-01 10:44:54 +0200

    Fix allSamplesAreForced property not being properly written.

--- MP42/MP42Muxer.m
+++ MP42/MP42Muxer.m
@@ -338,7 +338,7 @@
                 displayFlags = 0x20000000;
             if ([(MP42SubtitleTrack *)track someSamplesAreForced])
                 displayFlags += 0x40000000;
-            else if ([(MP42SubtitleTrack *)track allSamplesAreForced])
+            if ([(MP42SubtitleTrack *)track allSamplesAreForced])
                 displayFlags += 0xC0000000;
 
             MP4SetTrackIntegerProperty(_fileHandle, dstTrackId, "mdia.minf.stbl.stsd.tx3g.displayFlags", displayFlags);
