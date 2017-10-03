c88f0494e7066c6b9c002feab5c226b74dc53e49
Damiano Galassi <damiog@gmail.com>
2017-10-03 08:02:18 +0200

    MP42SrtImporter can open .smi too.

--- MP42/MP42SrtImporter.m
+++ MP42/MP42SrtImporter.m
@@ -24,7 +24,7 @@
 }
 
 + (NSArray<NSString *> *)supportedFileFormats {
-    return @[@"srt"];
+    return @[@"srt", @"smi"];
 }
 
 - (instancetype)initWithURL:(NSURL *)fileURL error:(NSError **)outError
