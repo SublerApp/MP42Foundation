9686d15e90e725ba260ebdc641144b12afc0949a
Andrew Hyatt <ahyattdev@icloud.com>
2018-04-19 13:40:11 -0400

    Generate a module, create umbrella header

---
+++ MP42/MP42Foundation.h
@@ -0,0 +1,14 @@
+//
+//  MP42Foundation.h
+//  MP42Foundation
+//
+//  Created by Andrew Hyatt on 4/19/18.
+//  Copyright © 2018 Damiano Galassi. All rights reserved.
+//
+
+#ifndef MP42Foundation_h
+#define MP42Foundation_h
+
+#import "MP42File.h"
+
+#endif /* MP42Foundation_h */
